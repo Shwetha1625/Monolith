4b8c43d7df2b9598e0f8daabd859e274cf915268
Shwetha1625 <swethanekkanti25@gmail.com>
2025-01-23 21:47:38 +0530

    optimised_browse__init__.py

--- optimised_browse__init__.py
+++ optimised_browse__init__.py
@@ -1,48 +1,37 @@
-import json
-from cart import dao
-import products
-from products import Product
+from products import dao
 
 
-class Cart:
-    def _init_(self, id: int, username: str, contents: list[Product], cost: float):
+class Product:
+    def __init__(self, id: int, name: str, description: str, cost: float, qty: int = 0):
         self.id = id
-        self.username = username
-        self.contents = contents
+        self.name = name
+        self.description = description
         self.cost = cost
+        self.qty = qty
 
-    @staticmethod
-    def load(data):
-        return Cart(data['id'], data['username'], data['contents'], data['cost'])
-
-
-def get_cart(username: str) -> list:
-    cart_details = dao.get_cart(username)
-    if cart_details is None:
-        return []
-    
-    # Assuming 'contents' is a JSON string or list-like object stored in the database
-    items = []
-    for cart_detail in cart_details:
-        # Directly parse the contents if it's a string representing a list or dictionary
-        contents = json.loads(cart_detail['contents']) if isinstance(cart_detail['contents'], str) else cart_detail['contents']
-        items.extend(contents)
-    
-    # Batch retrieve all products in one go to reduce multiple DB calls
-    product_ids = [item['id'] for item in items]  # Assuming item has 'id' key
-    products_list = products.get_products_by_ids(product_ids)
-    
-    # Map the evaluated contents to product objects
-    return [product for product in products_list if product.id in product_ids]
+    @classmethod
+    def from_dict(cls, data: dict):
+        """Alternative constructor to create a Product from a dictionary."""
+        return cls(data['id'], data['name'], data['description'], data['cost'], data['qty'])
 
 
-def add_to_cart(username: str, product_id: int):
-    dao.add_to_cart(username, product_id)
+def list_products() -> list[Product]:
+    """Fetch and return a list of Product objects."""
+    return [Product.from_dict(product) for product in dao.list_products()]
 
 
-def remove_from_cart(username: str, product_id: int):
-    dao.remove_from_cart(username, product_id)
+def get_product(product_id: int) -> Product:
+    """Fetch and return a single Product object by ID."""
+    return Product.from_dict(dao.get_product(product_id))
 
 
-def delete_cart(username: str):
-    dao.delete_cart(username)
+def add_product(product: dict):
+    """Add a new product using a dictionary."""
+    dao.add_product(product)
+
+
+def update_qty(product_id: int, qty: int):
+    """Update the quantity of a product by ID."""
+    if qty < 0:
+        raise ValueError('Quantity cannot be negative')
+    dao.update_qty(product_id, qty)
